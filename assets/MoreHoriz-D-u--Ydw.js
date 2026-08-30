f00511ec668e0538abc6f75baf3a32864f26705b
DESKTOP-QBONMG2\PC <ja_rod@hotmail.com>
2026-08-30 11:22:06 -0500

    hybrid meeting implementation

---
+++ assets/MoreHoriz-D-u--Ydw.js
@@ -0,0 +1,1 @@
+import{p as o,j as s}from"./index-DePRDSar.js";const t=o(s.jsx("path",{d:"M6 10c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2m12 0c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2m-6 0c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2"}),"MoreHoriz");export{t as M};
